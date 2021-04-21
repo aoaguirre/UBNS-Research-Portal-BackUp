93f4e134e2f61126fa9c445baa7a3fc542615834
Alexander Aguirre <aa284@buffalo.edu>
2021-04-20 21:55:59 -0400

    Initial Commit

---
+++ University at Buffalo Research Portal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  University at Buffalo Research Portal
+//
+//  Created by Alex Aguirre on 4/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ University at Buffalo Research PortalTests/University_at_Buffalo_Research_PortalTests.swift
@@ -0,0 +1,33 @@
+//
+//  University_at_Buffalo_Research_PortalTests.swift
+//  University at Buffalo Research PortalTests
+//
+//  Created by Alex Aguirre on 4/20/21.
+//
+
+import XCTest
+@testable import University_at_Buffalo_Research_Portal
+
+class University_at_Buffalo_Research_PortalTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
